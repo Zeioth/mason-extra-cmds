a4e9228be2c87226fcad81249f8774a1233c894d
Zeioth <zeioth@hotmail.com>
2024-03-10 20:21:26 +0100

    removed: Deprecated command MasonUpdate → It is already included in Mason.

--- lua/masonextracmds/init.lua
+++ lua/masonextracmds/init.lua
@@ -8,20 +8,6 @@ local M = {}
 function M.setup()
 
   -- Creaete extra Mason commands
-  cmd("MasonUpdate", function(options)
-    mason.update(options.fargs)
-  end, {
-    nargs = "*",
-    desc = "Update Mason Package",
-    complete = function(arg_lead)
-      local _ = require "mason-core.functional"
-      return _.sort_by(
-        _.identity,
-        _.filter(_.starts_with(arg_lead), require("mason-registry").get_installed_package_names())
-      )
-    end,
-  })
-
   cmd("MasonUpdateAll", function()
     mason.update_all()
   end, { desc = "Update Mason Packages" })
