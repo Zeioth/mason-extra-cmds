fc860b6bb8cb4654f4dec4d723839605dac1a3f2
Zeioth <zeioth@hotmail.com>
2024-03-10 19:44:39 +0100

    fix: missing utils.

--- lua/masonextracmds/utils.lua
+++ lua/masonextracmds/utils.lua
@@ -2,13 +2,41 @@
 
 local M = {}
 
+--- Merge extended options with a default table of options
+---@param default? table The default table that you want to merge into
+---@param opts? table The new options that should be merged with the default table
+---@return table # The merged table
+function M.extend_tbl(default, opts)
+  opts = opts or {}
+  return default and vim.tbl_deep_extend("force", default, opts) or opts
+end
+
 --- Serve a notification with a default title.
 ---@param msg string The notification body.
 ---@param type number|nil The type of the notification (:help vim.log.levels).
 ---@param opts? table The nvim-notify options to use (:help notify-options).
 function M.notify(msg, type, opts)
   vim.schedule(function() vim.notify(
-    msg, type, M.extend_tbl({ title = "Distroupdate.nvim" }, opts)) end)
+    msg, type, M.extend_tbl({ title = "mason.nvim" }, opts)) end)
+end
+
+--- Convenient wapper to save code when we Trigger events.
+---@param event string Name of the event.
+-- @usage To run a User event:   `trigger_event("User MyUserEvent")`
+-- @usage To run a Neovim event: `trigger_event("BufEnter")`
+function M.trigger_event(event)
+  -- detect if event start with the substring "User "
+  local is_user_event = string.match(event, "^User ") ~= nil
+
+  vim.schedule(function()
+    if is_user_event then
+      -- Substract the substring "User " from the beginning of the event.
+      event = event:gsub("^User ", "")
+      vim.api.nvim_exec_autocmds("User", { pattern = event, modeline = false })
+    else
+      vim.api.nvim_exec_autocmds(event, { modeline = false })
+    end
+  end)
 end
 
 return M
